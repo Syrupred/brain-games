cd2d8eb75685b7c4887f079bef14e53364e7b185
Ирина Харитонова <kuvirinaaa@gmail.com>
2021-10-27 21:59:36 +0300

    change index.js (delete import even/calc)

--- src/index.js
+++ src/index.js
@@ -1,19 +1,13 @@
 import readlineSync from 'readline-sync';
-import calc from './games/calc.js';
-import even from './games/even.js';
 
-export default () => {
-  const listOfGames = [calc, even];
-  const listOfPhrases = ['What is the result of the expression?', "Answer 'yes' if the number is even, otherwise answer 'no'."];
+export default (game, phrase) => {
   console.log('Welcome to the Brain Games!');
   const nameOfUser = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${nameOfUser}!`);
-  const selectedGame = readlineSync.question('Select game:\n Calc: 0 \n Even: 1\n');
-  const currentGame = listOfGames[+selectedGame];
-  console.log(listOfPhrases[+selectedGame]);
+  console.log(phrase);
 
   for (let numberOfCorrectAnswers = 0; numberOfCorrectAnswers < 3; numberOfCorrectAnswers += 1) {
-    const [question, correctResult] = currentGame();
+    const [question, correctResult] = game();
     const answer = readlineSync.question(`Question: ${question}\nYour answer: `);
     if (answer === correctResult) {
       console.log('Correct!');
