c6e3dc317f6afc157ff341a9cf4bb57e37dcc278
Ирина Харитонова <kuvirinaaa@gmail.com>
2021-10-28 21:45:40 +0300

    add brain-progression.js

---
+++ bin/brain-progression.js
@@ -0,0 +1,5 @@
+#!/usr/bin/env node
+import brainGames from '../src/index.js';
+import progression from '../src/games/progression.js';
+
+brainGames(progression, 'What number is missing in the progression?');
