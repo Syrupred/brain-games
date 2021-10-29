aa4aa17295f3be00faa0b19794f1d25c9bc24074
Ирина Харитонова <kuvirinaaa@gmail.com>
2021-10-29 18:49:49 +0300

    change index.js (add condition for brain-games)

--- src/index.js
+++ src/index.js
@@ -4,8 +4,12 @@ export default (game, phrase) => {
   console.log('Welcome to the Brain Games!');
   const nameOfUser = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${nameOfUser}!`);
-  console.log(phrase);
 
+  if (game === undefined) {
+    return '';
+  }
+
+  console.log(phrase);
   for (let numberOfCorrectAnswers = 0; numberOfCorrectAnswers < 3; numberOfCorrectAnswers += 1) {
     const [question, correctResult] = game();
     const answer = readlineSync.question(`Question: ${question}\nYour answer: `);
