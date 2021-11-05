3e711ff852bd2c8e2876c10f8beb5f85bd0fa770
Ирина Харитонова <kuvirinaaa@gmail.com>
2021-11-06 00:51:46 +0300

    delete phrase

--- bin/brain-prime.js
+++ bin/brain-prime.js
@@ -2,4 +2,4 @@
 import brainGames from '../src/index.js';
 import prime from '../src/games/prime.js';
 
-brainGames(prime, 'Answer "yes" if given number is prime. Otherwise answer "no".');
+brainGames(prime);
