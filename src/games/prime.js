6af7e7733398e958660f99843cb505058d5cd36d
Ирина Харитонова <kuvirinaaa@gmail.com>
2021-11-06 00:53:32 +0300

    change prime.js

--- src/games/prime.js
+++ src/games/prime.js
@@ -1,8 +1,27 @@
-import { getRandomNumber, isPrime } from '../functions.js';
+const getRandomNumber = (start, end) => {
+  const min = Math.ceil(start);
+  const max = Math.floor(end);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+};
 
-export default () => {
-  const randomNumber = getRandomNumber(1, 100);
+const isPrime = (randomNumber) => {
+  for (let divisor = 2; divisor < Math.sqrt(randomNumber); divisor += 1) {
+    if (randomNumber % divisor === 0) {
+      return 'no';
+    }
+  }
+  return 'yes';
+};
+
+const rulesGame = 'Answer "yes" if given number is prime. Otherwise answer "no".';
+
+const prime = () => {
+  const minRandomValue = 1;
+  const maxRandomValue = 100;
+  const randomNumber = getRandomNumber(minRandomValue, maxRandomValue);
   const question = `${randomNumber}`;
   const correctAnswer = isPrime(randomNumber);
   return [question, correctAnswer];
 };
+
+export default () => [prime, rulesGame];
