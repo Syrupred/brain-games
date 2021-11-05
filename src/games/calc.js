30d755df656e9d53ab31131e3df41f2b80a061c0
Ирина Харитонова <kuvirinaaa@gmail.com>
2021-11-06 00:53:01 +0300

    change calc.js

--- src/games/calc.js
+++ src/games/calc.js
@@ -1,10 +1,43 @@
-import { getRandomNumber, getRandomOperator, getResultOfCalc } from '../functions.js';
+const getRandomNumber = (start, end) => {
+  const min = Math.ceil(start);
+  const max = Math.floor(end);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+};
 
-export default () => {
-  const randomNumberFirst = getRandomNumber(1, 100);
-  const randomNumberSecond = getRandomNumber(1, 100);
+const getRandomOperator = (randomNumber) => {
+  const firstValue = 1;
+  const secondValue = 30;
+  const thirdValue = 65;
+  if (randomNumber >= firstValue && randomNumber < secondValue) {
+    return '*';
+  }
+  if (randomNumber >= secondValue && randomNumber <= thirdValue) {
+    return '+';
+  }
+  return '-';
+};
+
+const getResultOfCalc = (randomNumberFirst, randomNumberSecond, randomOperator) => {
+  if (randomOperator === '*') {
+    return randomNumberFirst * randomNumberSecond;
+  }
+  if (randomOperator === '+') {
+    return randomNumberFirst + randomNumberSecond;
+  }
+  return randomNumberFirst - randomNumberSecond;
+};
+
+const rulesGame = 'What is the result of the expression?';
+
+const calc = () => {
+  const minRandomValue = 1;
+  const maxRandomValue = 100;
+  const randomNumberFirst = getRandomNumber(minRandomValue, maxRandomValue);
+  const randomNumberSecond = getRandomNumber(minRandomValue, maxRandomValue);
   const randomOperator = getRandomOperator(randomNumberFirst);
   const question = `${randomNumberFirst} ${randomOperator} ${randomNumberSecond}`;
   const correctResult = getResultOfCalc(randomNumberFirst, randomNumberSecond, randomOperator);
   return [question, String(correctResult)];
 };
+
+export default () => [calc, rulesGame];
