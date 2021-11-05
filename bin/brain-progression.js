571c13aea6d178f96df0b6917f13e618f57ce086
Ирина Харитонова <kuvirinaaa@gmail.com>
2021-11-06 00:51:56 +0300

    delete phrase

--- bin/brain-progression.js
+++ bin/brain-progression.js
@@ -2,4 +2,4 @@
 import brainGames from '../src/index.js';
 import progression from '../src/games/progression.js';
 
-brainGames(progression, 'What number is missing in the progression?');
+brainGames(progression);
