aafea554614dc7c77b7de5276e32beedadc354f0
Ирина Харитонова <kuvirinaaa@gmail.com>
2021-11-06 00:53:44 +0300

    change progression.js

--- src/games/progression.js
+++ src/games/progression.js
@@ -1,12 +1,34 @@
-import { getRandomNumber, getProgression } from '../functions.js';
+const getRandomNumber = (start, end) => {
+  const min = Math.ceil(start);
+  const max = Math.floor(end);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+};
 
-export default () => {
-  const firstNumberOfProgression = getRandomNumber(1, 10);
-  const differenceOfProgression = getRandomNumber(1, 10);
-  const hiddenIndex = getRandomNumber(0, 9);
-  const progression = getProgression(firstNumberOfProgression, differenceOfProgression);
-  const correctAnswer = progression[hiddenIndex];
-  progression[hiddenIndex] = '..';
-  const question = `${progression.join(' ')}`;
+const getProgression = (firstNumberOfProgression, differenceOfProgression) => {
+  let numberOfProgression = 0;
+  const resultProgression = [];
+  for (let indexNumber = 1; indexNumber < 11; indexNumber += 1) {
+    numberOfProgression = firstNumberOfProgression + (indexNumber - 1) * differenceOfProgression;
+    resultProgression.push(numberOfProgression);
+  }
+  return resultProgression;
+};
+
+const rulesGame = 'What number is missing in the progression?';
+
+const progression = () => {
+  const minRandomValue = 1;
+  const maxRandomValue = 10;
+  const minHiddenIndex = 0;
+  const maxHiddenIndex = 9;
+  const firstNumberOfProgression = getRandomNumber(minRandomValue, maxRandomValue);
+  const differenceOfProgression = getRandomNumber(minRandomValue, maxRandomValue);
+  const hiddenIndex = getRandomNumber(minHiddenIndex, maxHiddenIndex);
+  const progressWithoutHidden = getProgression(firstNumberOfProgression, differenceOfProgression);
+  const correctAnswer = progressWithoutHidden[hiddenIndex];
+  progressWithoutHidden[hiddenIndex] = '..';
+  const question = `${progressWithoutHidden.join(' ')}`;
   return [question, String(correctAnswer)];
 };
+
+export default () => [progression, rulesGame];
