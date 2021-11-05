fa90e37d7803da048fa7dc940e628dbe64a0e412
Ирина Харитонова <kuvirinaaa@gmail.com>
2021-11-06 00:51:10 +0300

    delete phrase

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -2,4 +2,4 @@
 import brainGames from '../src/index.js';
 import even from '../src/games/even.js';
 
-brainGames(even, "Answer 'yes' if the number is even, otherwise answer 'no'.");
+brainGames(even);
