833e4ba61983ff7e182c4119ca677c657091f758
Ирина Харитонова <kuvirinaaa@gmail.com>
2021-11-06 00:53:22 +0300

    change gcd.js

--- src/games/gcd.js
+++ src/games/gcd.js
@@ -1,9 +1,33 @@
-import { getRandomNumber, getGreatestCommonDivisor } from '../functions.js';
+const getRandomNumber = (start, end) => {
+  const min = Math.ceil(start);
+  const max = Math.floor(end);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+};
 
-export default () => {
-  const randomNumberFirst = getRandomNumber(1, 100);
-  const randomNumberSecond = getRandomNumber(1, 100);
+const getGreatestCommonDivisor = (randomNumberFirst, randomNumberSecond) => {
+  let a = randomNumberFirst;
+  let b = randomNumberSecond;
+  if (a === b) {
+    return a;
+  }
+  if (a < b) {
+    const firstNumber = b;
+    b = a;
+    a = firstNumber;
+  }
+  return getGreatestCommonDivisor(a - b, b);
+};
+
+const rulesGame = 'Find the greatest common divisor of given numbers.';
+
+const gcd = () => {
+  const minRandomValue = 1;
+  const maxRandomValue = 100;
+  const randomNumberFirst = getRandomNumber(minRandomValue, maxRandomValue);
+  const randomNumberSecond = getRandomNumber(minRandomValue, maxRandomValue);
   const question = `${randomNumberFirst} ${randomNumberSecond}`;
   const correctAnswer = getGreatestCommonDivisor(randomNumberFirst, randomNumberSecond);
   return [question, String(correctAnswer)];
 };
+
+export default () => [gcd, rulesGame];
