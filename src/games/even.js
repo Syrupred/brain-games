3bd5a5c63b68220519b8e227c07dcfe444e3ffc5
Ирина Харитонова <kuvirinaaa@gmail.com>
2021-11-08 22:29:21 +0300

    change even.js

--- src/games/even.js
+++ src/games/even.js
@@ -1,19 +1,13 @@
-const getRandomNumber = (start, end) => {
-  const min = Math.ceil(start);
-  const max = Math.floor(end);
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-};
+import getRandomNumber from '../getRandomNumber.js';
 
 const rulesGame = "Answer 'yes' if the number is even, otherwise answer 'no'.";
 
-const even = () => {
-  const minRandomValue = 1;
-  const maxRandomValue = 100;
-  const randomNumber = getRandomNumber(minRandomValue, maxRandomValue);
+const gameRound = () => {
+  const randomNumber = getRandomNumber(1, 100);
   const question = `${randomNumber}`;
   const isEven = randomNumber % 2 === 0;
   const correctAnswer = isEven ? 'yes' : 'no';
   return [question, correctAnswer];
 };
 
-export default () => [even, rulesGame];
+export default () => [gameRound, rulesGame];
