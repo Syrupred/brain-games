8462a54946d6310a4c7c7ada76eb5e87cca0fe07
Ирина Харитонова <kuvirinaaa@gmail.com>
2021-11-13 22:31:27 +0300

    change even.js

--- src/games/even.js
+++ src/games/even.js
@@ -2,11 +2,12 @@ import getRandomNumber from '../getRandomNumber.js';
 
 const rulesGame = "Answer 'yes' if the number is even, otherwise answer 'no'.";
 
+const checkIsEven = (randomNumber) => randomNumber % 2 === 0;
+
 const gameRound = () => {
   const randomNumber = getRandomNumber(1, 100);
   const question = `${randomNumber}`;
-  const isEven = randomNumber % 2 === 0;
-  const correctAnswer = isEven ? 'yes' : 'no';
+  const correctAnswer = checkIsEven(randomNumber) ? 'yes' : 'no';
   return [question, correctAnswer];
 };
 
