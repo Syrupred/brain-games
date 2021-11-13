06bfe0661a0fbdd7706a3fdf4f6730635975ee4a
Ирина Харитонова <kuvirinaaa@gmail.com>
2021-11-13 22:30:49 +0300

    change progression.js

--- src/games/progression.js
+++ src/games/progression.js
@@ -3,9 +3,9 @@ import getRandomNumber from '../getRandomNumber.js';
 const getProgression = (start, step) => {
   let val = 0;
   const arrProgression = [];
-  const progressionLength = getRandomNumber(6, 11);
-  for (let i = 1; i < progressionLength; i += 1) {
-    val = start + (i - 1) * step;
+  const progressionLength = getRandomNumber(5, 10);
+  for (let i = 0; i < progressionLength; i += 1) {
+    val = start + i * step;
     arrProgression.push(val);
   }
   return arrProgression;
@@ -20,7 +20,7 @@ const gameRound = () => {
   const hiddenIndex = getRandomNumber(0, progression.length - 1);
   const correctAnswer = progression[hiddenIndex];
   progression[hiddenIndex] = '..';
-  const question = `${progression.join(' ')}`;
+  const question = progression.join(' ');
   return [question, String(correctAnswer)];
 };
 
